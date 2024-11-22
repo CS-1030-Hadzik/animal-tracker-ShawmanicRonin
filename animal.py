08a68b5b7f01c264e4dad610b92ac3a338231cd1
Ronin Evans <roninevans16@gmail.com>
2024-11-21 19:09:07 -0700

    Main.py runs

--- animal.py
+++ animal.py
@@ -5,13 +5,25 @@ class Animal:
     # Class-level attribute
     kingdom = "Animalia"
     # TODO create a class-level attribute that is a list of all the Animal objects
-
+    animals = []
     # TODO create the initializer for Animal with name and species as attributs
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        Animal.animals.append(self)
 
     # TODO: Add a method to make a generic sound 
+    @classmethod
+    def speak(self):
+        print("The animal makes a noise.")
     # Call the method `speak` and make it output a specific message like 
     # "The animal makes a noise.""
 
     # TODO __str__ method for string representation
+    def __str__(self):
+        return f'Kingdom {self.kingdom}, Name: {self.name} Species: {self.species}'
     # Example output
-    # Kingdom: 'kingdom attribute', Name: 'name attribute' Species: 'species attribute' 
+    # Kingdom: 'kingdom attribute', Name: 'name attribute' Species: 'species attribute'
+    @classmethod
+    def get_all_animals(cls):
+        return cls.animals
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-
+from animal import Animal
+from dog import Dog
 if __name__ == "__main__":
     # TODO: Create an instance of the Animal class
+    Generic = Animal('Generic', 'Unknown')
     # TODO: Print the Animal instance
+    print(Generic)
     # TODO: Call the method to make a generic animal sound
+    Animal.speak()
 
     # TODO: Create an instance of the Dog class
+    Buddy = Dog('Buddy', 'Canine', 'Golden Retriever')
     # TODO: Print the Dog instance
+    print(Buddy)
     # TODO: Call the method to make the dog-specific sound
-
+    Dog.speak()
     # TODO print out all the animals
+    all_animals = Animal.get_all_animals()
+    for animal in all_animals:
+        print(animal)
